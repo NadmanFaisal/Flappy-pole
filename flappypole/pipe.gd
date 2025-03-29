ac6cbcf5a7600e01e8d679a2491c7148782f1faa
Hasan Zahid <guszahidha@student.gu.se>
2025-03-29 14:40:32 +0100

    implemented pipe movement

--- flappypole/pipe.gd
+++ flappypole/pipe.gd
@@ -1,80 +1,80 @@
 extends Node2D
 
-const MOVE_SPEED = 1000.0  # constant speed
-const MIN_GROUP = 4
-const MAX_GROUP = 9
-const SPAWN_INTERVAL = 1.0  # in seconds
-const OFFSCREEN_X = -150  # pipe disappears after crossing left edge (adjust as needed)
+const MOVE_SPEED = 800.0
+const MIN_GROUP = 2
+const MAX_GROUP = 3
+const GROUP_PIPE_SPACING = 300  # spacing between groups
+const OFFSCREEN_X = -12000  # pipe fully off-screen before being removed
 
-var top_pipes = []
-var bottom_pipes = []
+var top_pipe_groups = []
+var bottom_pipe_groups = []
 var top_positions = []
 var bottom_positions = []
 
-var active_pipes = []
-var time_since_last_spawn = 0.0
+var active_groups = []
+var last_spawn_x = 0  # used to control spacing, not needed for now but kept for future use
 
 func _ready():
-	for i in range(1, 6):
-		var top_pipe = get_node("TopPipe%d" % i)
-		var bottom_pipe = get_node("BottomPipe%d" % i)
+	for i in range(1, 4):  # TopPipeGroup1 to 3, BottomPipeGroup1 to 3
+		var top_group = get_node("TopPipeGroup%d" % i)
+		var bottom_group = get_node("BottomPipeGroup%d" % i)
 
-		top_pipes.append(top_pipe)
-		top_positions.append(top_pipe.position)
+		top_pipe_groups.append(top_group)
+		top_positions.append(top_group.position)
 
-		bottom_pipes.append(bottom_pipe)
-		bottom_positions.append(bottom_pipe.position)
+		bottom_pipe_groups.append(bottom_group)
+		bottom_positions.append(bottom_group.position)
 
-		top_pipe.visible = false
-		bottom_pipe.visible = false
+		top_group.visible = false
+		bottom_group.visible = false
+
+	# Spawn the first group
+	spawn_group()
 
 func _process(delta):
-	var pipes_to_remove = []
+	var to_remove = []
 
-	# Move all active pipes
-	for pipe in active_pipes:
-		pipe.position.x -= MOVE_SPEED * delta
-		if pipe.position.x < OFFSCREEN_X:
-			pipe.visible = false
-			pipe.position = get_original_position(pipe)
-			pipes_to_remove.append(pipe)
+	# Move each active pipe group
+	for group in active_groups:
+		group.position.x -= MOVE_SPEED * delta
+		if group.position.x < OFFSCREEN_X:
+			group.visible = false
+			to_remove.append(group)
 
-	# Safely remove them after loop
-	for pipe in pipes_to_remove:
-		active_pipes.erase(pipe)
+	for group in to_remove:
+		active_groups.erase(group)
 
-	# Spawn new pipes on timer
-	time_since_last_spawn += delta
-	if time_since_last_spawn >= SPAWN_INTERVAL:
-		spawn_group()
-		time_since_last_spawn = 0.0
+	# Spawn a new group when there's room (distance-based)
+	if active_groups.size() == 0 or (active_groups[-1].position.x <= get_viewport_rect().size.x - GROUP_PIPE_SPACING):
+		if active_groups.size() < 10:
+			spawn_group()
 
 func spawn_group():
-	spawn_pipe_group(top_pipes, top_positions)
-	spawn_pipe_group(bottom_pipes, bottom_positions)
+	var use_top = randf() < 0.5
+	var source_list = top_pipe_groups if use_top else bottom_pipe_groups
+	var source_positions = top_positions if use_top else bottom_positions
 
-func spawn_pipe_group(pipe_list, position_list):
-	var max_available = min(MAX_GROUP, pipe_list.size())
-	var group_size = randi() % (max_available - MIN_GROUP + 1) + MIN_GROUP
+	spawn_pipe_group(source_list, source_positions)
 
-	var shuffled = pipe_list.duplicate()
-	shuffled.shuffle()
+func spawn_pipe_group(group_list, position_list):
+	var screen_width = get_viewport_rect().size.x
 
-	var screen_width = get_viewport_rect().size.x  # ✅ define screen width here
+	# Get only non-visible (available) pipe groups
+	var available = []
+	for group in group_list:
+		if not group.visible:
+			available.append(group)
+
+	if available.size() < MIN_GROUP:
+		return  # Not enough to spawn a full group
+
+	var group_size = randi() % (min(MAX_GROUP, available.size()) - MIN_GROUP + 1) + MIN_GROUP
+	available.shuffle()
 
 	for i in range(group_size):
-		var pipe = shuffled[i]
-		var original_pos = position_list[pipe_list.find(pipe)]
-		var spacing_x = i * randf_range(80, 140)
-		pipe.position = original_pos + Vector2(screen_width + spacing_x, 0)
-		pipe.visible = true
-		active_pipes.append(pipe)
-
-
-
-func get_original_position(pipe):
-	if top_pipes.has(pipe):
-		return top_positions[top_pipes.find(pipe)]
-	elif bottom_pipes.has(pipe):
-		return bottom_positions[bottom_pipes.find(pipe)]
-	return Vector2.ZERO
+		var group = available[i]
+		var original_pos = position_list[group_list.find(group)]
+		var spacing_x = i * GROUP_PIPE_SPACING
+		group.position = original_pos + Vector2(screen_width + spacing_x, 0)
+		group.visible = true
+		active_groups.append(group)
