1c80d620c341b55ce367e7fbd26f051c6f79e4c6
Nadman Faisal <nadman@chalmers.se>
2025-03-30 16:25:16 +0200

    Fix buttons

--- flappypole/Scripts/gameOverScript.gd
+++ flappypole/Scripts/gameOverScript.gd
@@ -2,12 +2,17 @@ extends CanvasLayer
 
 @onready var score_label = $Score
 @onready var replay = $Replay
+@onready var home = $HomeButton
 
 func _ready():
 	replay.pressed.connect(_replay_pressed)
+	home.pressed.connect(_home_button_pressed)
 
 func set_score(score: int):
 	score_label.text = "%d" % score
 
 func _replay_pressed():
 	get_tree().reload_current_scene()
+
+func _home_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/MenuScene.tscn")
